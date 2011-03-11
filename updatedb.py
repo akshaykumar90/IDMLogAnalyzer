79de049df465aab2b7f5b421198e6c39900eb2d3
Akshay Kumar <akshaykumar90@gmail.com>
2011-03-11 17:00:54 +0530

    Pretty progress bar; category & changelog support
    - NO error messages - not even missing log files
    - Replaced '*' with '#' to indicate any type of error
    - Added code to mark the category of the download depending upon
      the extension
    - Update the changelog table after updating the download table

--- updatedb.py
+++ updatedb.py
@@ -15,7 +15,8 @@ def main():
     conn = sqlite3.connect(db_filename)
     cursor = conn.cursor()
     
-    progress = 0;
+    progress    = 0;
+    valid_items = 0;
     
     try:
         for f in os.listdir(root_dir):
@@ -24,7 +25,11 @@ def main():
                 glob_path = os.path.join(sub_dir, "*.log")
                 globbed_files = glob.glob(glob_path)
                 if len(globbed_files) == 0:
-                    print "No log file exists in %s" % sub_dir
+                    # print "No log file exists in %s" % sub_dir
+                    print '#',
+                    progress = progress + 1
+                    if progress % 25 == 0:
+                        print progress
                     continue
                 log_file_path = globbed_files[0]
                 
@@ -48,17 +53,33 @@ def main():
                     name = m_name.group(1)
                     (filepath, filename) = os.path.split(name)
                     (shortname, extension) = os.path.splitext(filename)
+                    extension = extension[1:]
+
+                    # Select the category of the file based upon the extension
+                    # Compressed | Documents | Programs | Music | Video | General
+                    if extension in ['zip', 'rar', 'arj', 'gz', 'sit', 'sitx', 'sea', 'ace', 'bz2' '7z']:
+                        category = 'Compressed'
+                    elif extension in ['doc', 'pdf', 'ppt', 'pps']:
+                        category = 'Documents'
+                    elif extension in ['mp3', 'wav', 'wma', 'mpa', 'ram', 'ra', 'aac', 'aif', 'm4a']:
+                        category = 'Music'
+                    elif extension in ['exe', 'msi']:
+                        category = 'Programs'
+                    elif extension in ['avi', 'mpg', 'mpe', 'mpeg', 'asf', 'wmv', 'mov', 'qt', 'rm', 'mp4', 'flv']:
+                        category = 'Video'
+                    else:
+                        category = 'General'
                     
                     filesize = long(m_size.group(1))/1024
                     
-                    query = """insert into download (name, filetype, date, time, filesize) values (?, ?, ?, ?, ?)"""
-                    cursor.execute(query, (filename, extension, str(date), str(time), filesize))
-                    # print filename + " | " + extension + " | " + str(downloaded_on) + " | " + str(filesize)
+                    query = """insert into download (name, extension, category, date, time, filesize) values (?, ?, ?, ?, ?, ?)"""
+                    cursor.execute(query, (shortname, extension, category, str(date), str(time), filesize))
                     print '-',
+                    valid_items = valid_items + 1;
 
                     conn.commit()
                 else:
-                    print '*',
+                    print '#',
                     # print "Malformed Log File %s" % log_file_path
                     # if not m_time:
                         # print "m_time not found"
@@ -70,6 +91,12 @@ def main():
                 progress = progress + 1
                 if progress % 25 == 0:
                     print progress
+
+        print progress
+        query = """insert into changelog (update_date, new_items) values (?, ?)"""
+        print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " | " + str(valid_items)
+        cursor.execute(query, (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), valid_items))
+        conn.commit()
     finally:
         conn.close()
     
