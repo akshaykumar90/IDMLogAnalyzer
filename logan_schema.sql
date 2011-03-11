a7f80a37d5cddd7dde2ae094443af7f917aa9dcc
Akshay Kumar <akshaykumar90@gmail.com>
2011-03-11 16:55:44 +0530

    New changelog table and category field
    - Changelog table added to database to record updates to the
      database; Will support faster additions to the database in
      future
    - Category field added to download table to mark various types
      of downloads - Compressed, Video, etc.
    - filetype renamed to extension

--- logan_schema.sql
+++ logan_schema.sql
@@ -1,10 +1,19 @@
 -- LOGAN [LOG ANalyzer]
 -- Schema for IDM Log Database
 
+-- Changelog records the date when the database was updated and
+-- the no. of new items added
+create table changelog (
+    id              integer primary key autoincrement not null,
+    update_date     text,
+    new_items       integer
+);
+
 -- Download table holds all the entries stored in the log
 create table download (
     name            text,
-    filetype        text,
+    extension       text,
+    category        text,
     date            text,
     time            text,
     filesize        integer
