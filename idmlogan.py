1887f159039c95fa5738c1751e011c9eae850fd2
Akshay Kumar <akshaykumar90@gmail.com>
2011-03-19 15:54:05 +0530

    Added helper functions to SqliteInterface class
    - Rewrote SQL script generator function
    - __getDateTuple now returns datetime objects instead of strings
    - Added __getDateList function

--- idmlogan.py
+++ idmlogan.py
@@ -8,96 +8,124 @@ import datetime
 import sys
 
 class SqliteInterface:
-    def __init__(self, db_filename, timescale, category, **kwargs):
+    def __init__(self, db_filename, timescale, grouping, category, **kwargs):
         self.db_filename = db_filename
         self.timescale = timescale
+        self.grouping = grouping
         self.category = category
         if self.timescale != "all":
             self.period = kwargs['period']
 
-    def __SQLScriptGenerator(self, category=False, start_date=False, end_date=False, sum=True):
-        script_list = []
-        script_list.append("SELECT date,")
-        if sum:
-            script_list.append("SUM(filesize) AS fs")
+    def __generateSQLScript(self, select_time=False, category=False, start_date=False, \
+                            end_date=False, group_by_date=True, group_by_category=False):
+        select_sql   = "SELECT"
+        from_sql     = "FROM download"
+        where_sql    = "WHERE"
+        group_by_sql = "GROUP BY"
+        order_by_sql = "ORDER BY"
+        
+        select_clause = []
+        where_clause = []
+        group_by_clause = []
+        order_by_clause = []
+        
+        if group_by_date:
+            select_clause.append('date')
+            order_by_clause.append('date')
+            group_by_clause.append('date')
+        if select_time:
+            select_clause.append('time')
+        if group_by_date or group_by_category:
+            select_clause.append('SUM(filesize) AS fs')
         else:
-            script_list.append("filesize AS fs")
-        script_list.append("FROM download")
-        if start_date or end_date or category:
-            script_list.append("WHERE")
-            where_clause = []
-            if start_date:
-                where_clause.append("date > ?")
-            if end_date:
-                where_clause.append("date < ?")
-            if category:
-                where_clause.append("category = ?")
-            script_list.append(' AND '.join(where_clause))
-        if sum:
-            script_list.append("GROUP BY date")
-        script_list.append("ORDER BY")
-        order_clause = []
-        order_clause.append("date")
-        if not sum:
-            order_clause.append("time")
-        script_list.append(','.join(order_clause))
-        return " ".join(script_list)
+            select_clause.append('filesize AS fs')
+        if group_by_category:
+            select_clause.append('category')
+            group_by_clause.append('category')
+            order_by_clause.append('category')
+        if start_date:
+            where_clause.append('date > ?')
+        if end_date:
+            where_clause.append('date < ?')
+        if category:
+            where_clause.append('category = ?')
+        if not group_by_date and not group_by_category:
+            order_by_clause.append('time')
+        
+        if not where_clause: where_sql = ""
+        if not group_by_clause: group_by_sql = ""
+        
+        return " ".join([select_sql, ",".join(select_clause),     \
+                        from_sql,                                 \
+                        where_sql, " AND ".join(where_clause),    \
+                        group_by_sql, ",".join(group_by_clause),  \
+                        order_by_sql, ",".join(order_by_clause)])
 
-    def __getDateValues(self, format, start, delta=1):
+    def __getDateTuple(self, format, start, delta=1):
         if format == "year":
             y = int(start)
-            start_date = "%d-01-01" % (y,)
-            end_date = "%d-01-01" % (y+delta,)
+            start_date_str = "%d-01-01" % (y,)
+            start_date = datetime.datetime.strptime(start_date_str, "%Y-%m-%d")
+            end_date_str = "%d-01-01" % (y+delta,)
+            end_date = datetime.datetime.strptime(end_date_str, "%Y-%m-%d")
         elif format == "month":
             month, year = start.split("/")
             m = int(month)
             y = int(year)
-            start_date = "%d-%02d-01" % (y, m)
+            start_date_str = "%d-%02d-01" % (y, m)
+            start_date = datetime.datetime.strptime(start_date_str, "%Y-%m-%d")
             m = m + delta
             y = y + (m / 12)
-            end_date = "%d-%02d-01" % (y, m)
+            end_date_str = "%d-%02d-01" % (y, m)
+            end_date = datetime.datetime.strptime(end_date_str, "%Y-%m-%d")
         elif format == "week":
-            start_date_obj = datetime.datetime.strptime(start, "%m-%d-%Y")
+            start_date = datetime.datetime.strptime(start, "%m-%d-%Y")
             one_week = datetime.timedelta(days=7*delta)
-            end_time_obj = start_date_obj + one_week
-            start_date = start_date_obj.strftime("%Y-%m-%d")
-            end_date = end_time_obj.strftime("%Y-%m-%d")
+            end_time = start_date_obj + one_week
 
         return (start_date, end_date)
+    
+    def __getDateList(self, start_date, end_date, delta):
+        date_list = []
         
-    def getXY(self):
+        if delta == "month":
+            n = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month) + 2
+            month = start_date.month
+            year = start_date.year
+            for i in range(n):
+                date_list.append("%d-%02d-01" % (year, month))
+                month = month + 1
+                if (month > 12):
+                    month = 1
+                    year = year + 1
+        elif delta == "year":
+            n = end_date.year - start_date.year + 2
+            year = start_date.year
+            for i in range(n):
+                date_list.append("%d-01-01" % (year,))
+                year = year + 1
+        
+        return date_list
+        
+    def getData(self):
         conn = sqlite3.connect(self.db_filename)
         cursor = conn.cursor()
         
-        start_date_req = False
-        end_date_req = False
-        category_req = False
-        
-        parameter_list = []
-        
         if self.timescale != "all":
-            start_date_req = True
-            end_date_req = True
-            start_date, end_date = __getDateValues(self.timescale, self.period, delta=1)
-            parameter_list.extend([start_date, end_date])
-            
-        if self.category != "all":
-            category_req = True
-            parameter_list.append(self.category)
-            
-        query = self.__SQLScriptGenerator(category_req, start_date_req, end_date_req)
-        cursor.execute(query, tuple(parameter_list))
+            timescale_lower, timescale_upper = self.__getDateTuple(self.timescale, self.period)
+        else:
+            cursor.execute("""SELECT date FROM download ORDER BY date LIMIT 1""")
+            timescale_lower, = cursor.fetchone()
+            cursor.execute("""SELECT date FROM download ORDER BY date DESC LIMIT 1""")
+            timescale_upper, = cursor.fetchone()
         
-        data_all = [(date, value) for (date, value) in cursor.fetchall()]
+        if self.grouping == "month" or self.grouping == "year":
+            date_list = self.__getDateList(timescale_lower, timescale_upper, self.grouping)
+            n_queries = len(date_list) - 1
+        else:
+            n_queries = 1
         
         conn.close()
-        
-        fmtstring = "%Y-%m-%d"
-
-        x = [mpldates.date2num(datetime.datetime.strptime(date, fmtstring)) for (date,value) in data_all]
-        y = [value for (date,value) in data_all]
-        
-        return (x,y)
 
 class LineChart:
     def __init__(self, timescale):
