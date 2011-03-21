bf1aa1c150ad2b8ca2e60da82b87cc3711875261
Akshay Kumar <akshaykumar90@gmail.com>
2011-03-21 15:55:28 +0530

    getData complete upto query/argument generation
    - getData now generates all the required queries with their argument
      tuple for the data model requested correctly
    - Switched default value for time_wise_grouping to date
    - Modified __getDateList to handle raw dates from database as well

--- idmlogan.py
+++ idmlogan.py
@@ -8,7 +8,7 @@ import datetime
 import sys
 
 class SqliteInterface:
-    def __init__(self, db_filename, timescale='all', category='all', time_wise_grouping='no', \
+    def __init__(self, db_filename, timescale='all', category='all', time_wise_grouping='date', \
                  category_wise_grouping=False, **kwargs):
         self.db_filename = db_filename
         self.timescale = timescale
@@ -91,15 +91,17 @@ class SqliteInterface:
         elif format == "week":
             start_date = datetime.datetime.strptime(start, "%m-%d-%Y")
             one_week = datetime.timedelta(days=7*delta)
-            end_time = start_date_obj + one_week
+            end_date = start_date + one_week
 
         return (start_date, end_date)
     
-    def __getDateList(self, start_date, end_date, delta):
+    def __getDateList(self, start_date, end_date, delta, raw_dates=False):
         date_list = []
         
         if delta == "month":
-            n = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month) + 2
+            n = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month) + 1
+            if raw_dates:
+                n = n + 1
             month = start_date.month
             year = start_date.year
             for i in range(n):
@@ -109,7 +111,9 @@ class SqliteInterface:
                     month = 1
                     year = year + 1
         elif delta == "year":
-            n = end_date.year - start_date.year + 2
+            n = end_date.year - start_date.year + 1
+            if raw_dates:
+                n = n + 1
             year = start_date.year
             for i in range(n):
                 date_list.append("%d-01-01" % (year,))
@@ -121,29 +125,70 @@ class SqliteInterface:
         conn = sqlite3.connect(self.db_filename)
         cursor = conn.cursor()
         
+        # Defaults for __generateSQLScript
+        category    = False
+        start_date  = False
+        end_date    = False
+        group_files = False
+        group_by    = None
+        
         if self.timescale != "all":
             timescale_lower, timescale_upper = self.__getDateTuple(self.timescale, self.period)
             if self.time_wise_grouping == "month" or self.time_wise_grouping == "year":
+                # Dataset No. 4
                 date_list = self.__getDateList(timescale_lower, timescale_upper, self.time_wise_grouping)
                 n_queries = len(date_list) - 1
+                group_files = True
             else:
-                date_list = [ timescale_lower, timescale_upper ]
+                # Dataset No. 1 & 2
+                date_list = [ timescale_lower.strftime("%Y-%m-%d"), timescale_upper.strftime("%Y-%m-%d") ]
                 n_queries = 1
+                if self.time_wise_grouping == 'date':
+                    # Dataset No. 1
+                    group_files = True
+                    group_by = 'date'
         elif self.time_wise_grouping == "month" or self.time_wise_grouping == "year":
+            # Dataset No. 4
             cursor.execute("""SELECT date FROM download ORDER BY date LIMIT 1""")
-            timescale_lower, = cursor.fetchone()
+            timescale_lower_str, = cursor.fetchone()
+            timescale_lower = datetime.datetime.strptime(timescale_lower_str, "%Y-%m-%d")
             cursor.execute("""SELECT date FROM download ORDER BY date DESC LIMIT 1""")
-            timescale_upper, = cursor.fetchone()
-            date_list = self.__getDateList(timescale_lower, timescale_upper, self.time_wise_grouping)
+            timescale_upper_str, = cursor.fetchone()
+            timescale_upper = datetime.datetime.strptime(timescale_upper_str, "%Y-%m-%d")
+            date_list = self.__getDateList(timescale_lower, timescale_upper, self.time_wise_grouping, raw_dates=True)
             n_queries = len(date_list) - 1
+            group_files = True
         else:
+            # Dataset No. 1 & 2
             date_list = None
             n_queries = 1
-            
+            if self.time_wise_grouping == 'date':
+                # Dataset No. 1
+                group_files = True
+                group_by = 'date'
+        
         if self.category == "all":
             category = False
         else:
             category = True
+            category_list = [self.category] * n_queries
+        
+        if self.category_wise_grouping:
+            if self.time_wise_grouping == 'month' or self.time_wise_grouping == 'year':
+                # Dataset No. 6
+                group_files = True
+                group_by = 'category'
+            elif self.time_wise_grouping == 'date':
+                # Dataset No. 5
+                n_queries = 6
+                group_files = True
+                group_by = 'date'
+                category = True
+                category_list = ['Compressed', 'Documents', 'General', 'Music', 'Programs', 'Video']
+            else:
+                # Dataset No. 3
+                group_files = True
+                group_by = 'category'
         
         if date_list:
             start_date = True
@@ -152,30 +197,20 @@ class SqliteInterface:
             start_date = False
             end_date = False
         
-        if self.time_wise_grouping == 'no':
-            select_time = True
-            group_by_category = False
-            group_by_date = False
-        elif self.time_wise_grouping == 'date':
-            select_time = False
-            group_by_date = True
-            sum_filesize = True
-        else:
-            select_time = False
-            group_by_date = False
-            sum_filesize = True
-        
-        if self.category_wise_grouping:
-            group_by_category = True
-            group_by_date = False
-        else:
-            group_by_category = False
-            group_by_date = False
-        
-        if self.time_wise_grouping == 'no' and self.timescale == 'week':
-            group_by_date = False
-            select_time = True
+        for i in range(n_queries):
+            arguments = []
             
+            if start_date and end_date:
+                arguments.extend([date_list[i], date_list[i+1]])
+            
+            if category:
+                arguments.append(category_list[i])
+            
+            queryStr = self.__generateSQLScript(category, start_date, end_date, group_files, group_by=group_by)
+            arg_tuple = tuple(arguments)
+            # cursor.execute(queryStr, arg_tuple)
+            print queryStr, ',', arg_tuple
+        
         conn.close()
 
 class LineChart:
