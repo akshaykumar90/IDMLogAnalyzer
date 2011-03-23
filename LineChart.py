ae304afb5cbd2ba44a454f4cb0953e718165eefb
Akshay Kumar <akshaykumar90@gmail.com>
2011-03-23 20:01:58 +0530

    LineChart class now draws 4 types of line plots
    - yaxis major formatter and locator configured to draw pretty
      graphs; added __scale_yvalues helper function
    - Class initializer now takes 'timescale' as a keyword argument

--- LineChart.py
+++ LineChart.py
@@ -2,13 +2,31 @@ import matplotlib.dates as mpldates
 import matplotlib.pyplot as plt
 from matplotlib.dates import MonthLocator, WeekdayLocator, DateFormatter, DayLocator, HourLocator
 from matplotlib.dates import MONDAY
+from matplotlib.ticker import MultipleLocator, FuncFormatter
 
 class LineChart:
-    def __init__(self, xvalues, yvalues, timescale, use_plot_date):
+    def __init__(self, xvalues, yvalues, use_plot_date, **kwargs):
         self.xvalues = xvalues
         self.yvalues = yvalues
-        self.timescale = timescale
         self.use_plot_date = use_plot_date
+        if use_plot_date:
+            self.timescale = kwargs['timescale']
+    
+    def __scale_yvalues(self, ymax):
+        ONE_GB              = 1048576
+        SCALE_ALLOWED       = [1, 2, 4, 8, 10]
+        MAX_TICKS_ALLOWED   = 15
+        
+        # GB ticks required for ymax
+        ticks_req = int( ymax / ONE_GB ) + 1
+        
+        for i in SCALE_ALLOWED:
+            if ticks_req / i > MAX_TICKS_ALLOWED:
+                continue
+            else:
+                break
+        
+        return i
     
     def draw(self):
         fig = plt.figure()
@@ -40,8 +58,19 @@ class LineChart:
                 ax.xaxis.set_minor_locator(hours)
         else:
             ax.plot(self.yvalues)
+            ax.set_xticks(range(len(self.xvalues)))
             ax.set_xticklabels(self.xvalues)
+            
+        def format_func(x, pos):
+            return x / 1048576 # = 1024 * 1024 KB = 1 GB
         
+        l, u = ax.get_ylim()
+        GB_scale_req = self.__scale_yvalues(u)
+        GB_Locator = MultipleLocator(1048576 * GB_scale_req)
+        
+        ax.yaxis.set_major_locator(GB_Locator)
+        formatter = FuncFormatter(format_func)
+        ax.yaxis.set_major_formatter(formatter)
         ax.autoscale_view()
         ax.grid(True)
         fig.autofmt_xdate()
