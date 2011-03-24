d30d0b7be82d2a1f438b367c7e343ff98c87eb11
Akshay Kumar <akshaykumar90@gmail.com>
2011-03-24 13:57:05 +0530

    Add BarChart class to chartlib
    - BarChart class supports plotting normal bar graphs as well as
      stacked bar graphs

--- chartlib.py
+++ chartlib.py
@@ -71,8 +71,70 @@ class LineChart:
         ax.yaxis.set_major_locator(GB_Locator)
         formatter = FuncFormatter(format_func)
         ax.yaxis.set_major_formatter(formatter)
+        
         ax.autoscale_view()
         ax.grid(True)
         fig.autofmt_xdate()
 
+        plt.show()
+
+class BarChart:
+    def __init__(self, xvalues, yvalues, stacked_bar_graph=False):
+        self.xvalues = xvalues
+        self.yvalues = yvalues
+        self.stacked_bar_graph = stacked_bar_graph
+    
+    def __scale_yvalues(self, ymax):
+        ONE_GB              = 1048576
+        SCALE_ALLOWED       = [1, 2, 4, 8, 10]
+        MAX_TICKS_ALLOWED   = 15
+        
+        # GB ticks required for ymax
+        ticks_req = int( ymax / ONE_GB ) + 1
+        
+        for i in SCALE_ALLOWED:
+            if ticks_req / i > MAX_TICKS_ALLOWED:
+                continue
+            else:
+                break
+        
+        return i
+    
+    def draw(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        
+        width = 0.8
+        locs = range(1, len(self.xvalues) + 1)
+        if self.stacked_bar_graph == False:
+            ax.bar(locs, self.yvalues, width=width)
+        else:
+            colors = ['b','g','c','m','r','y']
+            for i in range(len(self.xvalues)):
+                bottom = 0
+                k = 0    
+                for j in self.yvalues[i]:
+                    ax.bar(i+1, j, bottom=bottom, width=width, color = colors[k])
+                    bottom = bottom + j
+                    k = k + 1
+        
+        ticks_loc = [i + width / 2 for i in locs]
+        ax.set_xticks(ticks_loc)
+        ax.set_xticklabels(self.xvalues)
+        
+        def format_func(x, pos):
+            return x / 1048576 # = 1024 * 1024 KB = 1 GB
+        
+        l, u = ax.get_ylim()
+        GB_scale_req = self.__scale_yvalues(u)
+        GB_Locator = MultipleLocator(1048576 * GB_scale_req)
+        
+        ax.yaxis.set_major_locator(GB_Locator)
+        formatter = FuncFormatter(format_func)
+        ax.yaxis.set_major_formatter(formatter)
+        
+        ax.autoscale_view()
+        ax.grid(True)
+        fig.autofmt_xdate()
+        
         plt.show()
