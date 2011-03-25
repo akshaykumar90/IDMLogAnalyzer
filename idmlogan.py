b5e58a992240b5f8ef587dbbfa077b6d0fda0588
Akshay Kumar <akshaykumar90@gmail.com>
2011-03-25 13:05:13 +0530

    Add command line interface for the main script
    - Command line arguments handling code is added
    - SqliteInterface and Line/Bar/Pie classes are created and handled
      to draw appropriate plots
    - Controller class dropped

--- idmlogan.py
+++ idmlogan.py
@@ -1,45 +1,79 @@
 import getopt
 import sys
-
-class Controller:
-    def __init__(self, chart_type, data_model, **kwargs):
-        self.chart_type = chart_type
-        self.data_model = data_model
-    pass
+from SqliteInterface import SqliteInterface
+import chartlib
 
 def main(argv):
+    LINE_PLOT = 0
+    BAR_PLOT  = 1
+    PIE_PLOT  = 2
+    
+    timescale = 'all'
+    time_wise_grouping = 'date'
+    category_wise_grouping = False
+    category = 'all'
+    sqli_kwargs = {}
+    db_filename = 'logan.db'
+    
     try:
-        opts, args = getopt.getopt(argv, "hlbpay:m:", ["help", "line", "bar", "pie", "all", "year=", "month="])
+        opts, args = getopt.getopt(argv, "hlbpay:m:w:g:c:d:", \
+        ["help", "line", "bar", "pie", "all", "year=", "month=", "week=", "group=", "category=", "database="])
     except getopt.GetoptError:
         sys.exit(2)
     
     for opt,arg in opts:
         if opt in ("-l", "--line"):
             # Line Chart
-            line = True
+            chart_type = LINE_PLOT
         elif opt in ("-b", "--bar"):
             # Bar Chart
-            pass
+            chart_type = BAR_PLOT
         elif opt in ("-p", "--pie"):
             # Pie Chart
-            pass
+            chart_type = PIE_PLOT
         elif opt in ("-a", "--all"):
             # Plot All Time data
             timescale = "all"
         elif opt in ("-m", "--month"):
             # Plot only for `arg` month
             timescale = "month"
-            period = arg
+            sqli_kwargs['period'] = arg
         elif opt in ("-y", "--year"):
             # Plot only for `arg` year
             timescale = "year"
-            period = arg
+            sqli_kwargs['period'] = arg
+        elif opt in ("-w", "--week"):
+            # Plot only for `arg` week
+            timescale = "week"
+            sqli_kwargs['period'] = arg
+        elif opt in ("-g", "--group"):
+            group_by = arg.split(',')
+            if 'category' in group_by:
+                category_wise_grouping = True
+                group_by.remove('category')
+            time_wise_grouping = group_by.pop()
+        elif opt in ("-c", "--category"):
+            category = arg
+        elif opt in ("-d", "--database"):
+            db_filename = arg
     
-    if line:
-        if timescale == "all":
-            lc = LineChart("logan.db", timescale)
-        else:
-            lc = LineChart("logan.db", timescale, time=period)
+    data_model = SqliteInterface(db_filename, timescale, category, time_wise_grouping, \
+                                 category_wise_grouping, **sqli_kwargs)
+    xvalues, yvalues = data_model.getData()
+    
+    if chart_type == LINE_PLOT:
+        lc_kwargs = {}
+        use_plot_date = data_model.get_xvalues_type()
+        if use_plot_date:
+            lc_kwargs['timescale'] = timescale
+        lc = chartlib.LineChart(xvalues, yvalues, use_plot_date, **lc_kwargs)
         lc.draw()
+    elif chart_type == BAR_PLOT:
+        stacked_bar_graph = data_model.get_yvalues_type()
+        bc = chartlib.BarChart(xvalues, yvalues, stacked_bar_graph)
+        bc.draw()
+    elif chart_type == PIE_PLOT:
+        pc = chartlib.PieChart(xvalues, yvalues)
+        pc.draw()
 
-# if __name__=="__main__": main(sys.argv[1:])
+if __name__=="__main__": main(sys.argv[1:])
